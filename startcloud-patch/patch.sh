63ff667fad28bfc467b9f5bd4ce243bd35fb99f2
Yanwei Yao <yanwei.yao@me.com>
2017-05-02 22:20:55 +0800

    add cust patch for local repo

---
+++ startcloud-patch/patch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: patch.sh
+# 
+#         USAGE: ./patch.sh 
+# 
+#   DESCRIPTION: customizing the local repo
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Yanwei Yao (yyanwei@cn.ibm.com)
+#  ORGANIZATION: 
+#       CREATED: 05/02/2017 22:03
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+cp -f install_on_debian.yml /etc/ansible/roles/ceph.ceph-common/tasks/installs/install_on_debian.yml
+cp -f ceph_all.yml /etc/ansible/roles/ceph_client/tasks/ceph_all.yml
+cp -f galera_client_install_apt.yml /etc/ansible/roles/galera_client/tasks/galera_client_install_apt.yml
+cp -f galera_install_apt.yml /etc/ansible/roles/galera_server/tasks/galera_install_apt.yml
