359a1d292a91f2bcab1ee99b08641d68645a6a2b
Yanwei Yao <yanwei.yao@me.com>
2017-06-23 20:42:57 +0800

    update startcloud patch

--- startcloud-patch/patch.sh
+++ startcloud-patch/patch.sh
@@ -20,6 +20,6 @@
 set -o nounset                              # Treat unset variables as an error
 cp -f install_on_debian.yml /etc/ansible/roles/ceph.ceph-common/tasks/installs/install_on_debian.yml
 cp -f ceph_all.yml /etc/ansible/roles/ceph_client/tasks/ceph_all.yml
-cp -f galera_client_install_apt.yml /etc/ansible/roles/galera_client/tasks/galera_client_install_apt.yml
+#cp -f galera_client_install_apt.yml /etc/ansible/roles/galera_client/tasks/galera_client_install_apt.yml
 cp -f galera_install_apt.yml /etc/ansible/roles/galera_server/tasks/galera_install_apt.yml
 cp -f pre_install_apt.yml /etc/ansible/roles/pip_install/tasks/pre_install_apt.yml
